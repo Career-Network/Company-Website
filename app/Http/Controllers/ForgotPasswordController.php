d8bae00684344b12be24690a7355eb447a3a618b
Andreas Prasetya <72151341+andreasprass@users.noreply.github.com>
2022-07-29 10:57:49 +0700

    Add function reset password

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -6,8 +6,49 @@ use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
-    public function index()
-    {
+    public function password_request(){
+
         return view('forgot-password');
+    }
+
+    public function password_email(Request $request){
+        $request->validate(['email' => 'required|email:dns']);
+ 
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+ 
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
+    }
+
+    public function password_reset($token){
+        return view('reset-password', ['token' => $token]);
+    }
+
+    public function password_update(Request $request){
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+     
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+     
+                $user->save();
+     
+                event(new PasswordReset($user));
+            }
+        );
+     
+        return $status === Password::PASSWORD_RESET
+                    ? redirect()->route('login')->with('status', __($status))
+                    : back()->withErrors(['email' => [__($status)]]);
     }
 }
