52eabb4ce0fe0acf3915ecb87986d0f91453a97a
andreasprass <andreasprasetya29@gmail.com>
2022-07-31 20:58:23 +0700

    Nama user login di dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,17 @@
 
 @extends('layouts.navbar_footer',['title'=>'Career Network'])
 @section('content')
-      <div class="heading">
-        <p class="title">Selamat Datang di Halaman Utama</p>
-        {{-- <p>{{ Auth::user()->name }}</p> --}}
+    <main>
+      <div class="heading" style="margin-top: 170px">
         @auth
-        <p>This button only show up when user logged in!</p>
-        <a class="btn btn-primary" href="">Just Button</a>
+          <p class="title">Selamat Datang {{ Auth::user()->name }}</p>
+          <p>This button only show up when user logged in!</p>
+          <a class="btn btn-primary" href="">Just Button</a>
+        @else
+          <p class="title">Selamat Datang di Halaman Utama</p>
         @endauth
       </div>
-    
+    </main>
 @endsection
     
 
