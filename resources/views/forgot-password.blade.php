cac7c2296f269a9234d675bebd3644d404a5d285
Hans Evan Tatipata <thansevan@gmail.com>
2022-08-02 02:03:52 +0700

    modularization css

--- resources/views/forgot-password.blade.php
+++ resources/views/forgot-password.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.navbar_footer',[
   'title'=>'Reset Password',
-  'css'=>"assets/css/info_pass.css"
+  'css'=>"assets/css/forgot_pass.css"
   ])
 @section('content')
   <main>
@@ -18,8 +18,21 @@
     
     <div class="main__card">
       <div class="heading">
-        <h2 class="sh_1 fw_bold text_primary">Terkirim</h2>
-        <p class="bt_4 fw_regular text_grey">Kami telah mengirimkan link ke email kamu yang dapat digunakan untuk mengatur ulang kata sandi. Jika email tidak ditemukan jangan lupa untuk mencarinya di folder spam dan sampah.</p>
+        <h2 class="sh_1 fw_bold text_primary">Lupa Kata Sandi?</h2>
+        <p class="bt_4 fw_regular text_grey">Masukkan alamat email yang terdaftar di akun Career Network dan kami akan mengirimkan email untuk mengatur ulang kata sandi.</p>
+      </div>
+      <div class="form__group">
+        <form action="{{ route('password_email') }}" method="post">
+          @csrf
+          <label class="bt_3 fw_medium">Alamat Email <span class="text_danger">*</span> </label>
+          <input type="email" name="email" class="form-control @error('email') is-invalid @enderror">
+          @error('email')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+          @enderror
+          <button type="submit" class="btn">Send Password Reset Link</button>
+        </form>
       </div>
     </div>
   </main>
