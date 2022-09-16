d15495d36e4bc6a90d99d37bf6346a69044037ac
andreasprass <andreasprasetya29@gmail.com>
2022-09-16 12:21:34 +0700

    merapikan controller dan routes

--- routes/web.php
+++ routes/web.php
@@ -23,54 +23,25 @@ use App\Http\Controllers\ForgotPasswordController;
 |
 */
 
+//Pages
 Route::get('/', [DashboardController::class, 'index'])->name('home')->withoutMiddleware('auth');
 Route::get('/home', [DashboardController::class, 'index'])->name('home')->withoutMiddleware('auth');
 Route::get('/educareer', function () {
     return view('list_class');
 })->name('educareer');
 
+//Login feature
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'auth'])->name('auth');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+//Register feature
 Route::get('/register', [RegisterController::class, 'index'])->name('register')->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store'])->name('store');
 
+//Forgot passsword feature
 Route::get('/forgot-password', [ForgotPasswordController::class, 'forgot_view'])->name('password_request')->middleware('guest');
 Route::post('/forgot-password', [ForgotPasswordController::class, 'password_email'])->name('password_email')->middleware('guest');
+Route::get('/reset-password{token}',[ForgotPasswordController::class, 'password_reset'])->name('password.reset')->middleware('guest');
+Route::post('/reset-password', [ForgotPasswordController::class, 'password_update'])->name('password_update')->middleware('guest');
 
-//taruh sini bentar, belum dirapikan ke controller, sekalian belajar
-Route::get('/reset-password/{token}', function ($token) {
-    return view('reset-password', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
-
-Route::post('/reset-password', function (Request $request) {
-    $request->validate([
-        'token' => 'required',
-        'email' => 'required|email:dns',
-        'password' => [
-            'required',
-            'min:8',
-            'regex:/[a-z]/',
-            'regex:/[A-Z]/',
-            'regex:/[0-9]/',
-        ],
-        'password_confirmation' => 'same:password'
-    ]);
-
-    $status = Password::reset(
-        $request->only('email', 'password', 'password_confirmation', 'token'),
-        function ($user, $password) {
-            $user->forceFill([
-                'password' => Hash::make($password)
-            ])->setRememberToken(Str::random(60));
-
-            $user->save();
-
-            event(new PasswordReset($user));
-        }
-    );
-    return $status === Password::PASSWORD_RESET
-        ? view('info_reset')
-        : back()->withErrors(['email' => [__($status)]]);
-})->middleware('guest')->name('password_update');
