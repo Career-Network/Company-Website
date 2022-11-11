5ef0c5ab27637ec924ade7c2751f160aa8c318f8
widyalestari9 <88524887+widyalestari9@users.noreply.github.com>
2022-11-11 20:51:08 +0700

    list mentor

---
+++ resources/views/pilih_mentor.blade.php
@@ -0,0 +1,185 @@
+@extends('layouts.navbar_footer',[
+  'title'=>'Edu Career',
+  'css'=> 'assets/css/pilih_mentor.css'
+  ])
+  <!-- <style>
+    .swiper-backface-hidden .swiper-slide{
+        width: fit-content;
+    }
+  </style> -->
+@section('content')
+<main>
+    <section class="hero-section"> 
+
+        <div class="container">
+                <div class="section">
+                    <h4>Pilih Mentormu Disini!</h4>
+                    <p class="text">Silahkan kamu pilih mentor yang sesuai dengan kriteriamu.</p>
+                    
+                </div>
+
+        </div>
+
+
+    </section>
+    <!-- End Hero Sction -->
+
+     <!-- produk service -->
+   <section class="service">
+    <div class="container"> 
+          <div class="row mt-5">
+
+            <div class="col-md-4">
+              <div class="text-center">
+                <img src="img/mentor1.png" alt="mentor" class="img-mentor">
+                <p class="nama-mentor">Nama Mentor</p>
+                <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                    veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                    <div class="text-center">
+                        <a href="#" class="btn btn-success">
+                          Gabung Kelas
+                        </a>
+                      </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="text-center">
+                  <img src="img/mentor2.png" alt="mentor" class="img-mentor">
+                  <p class="nama-mentor">Nama Mentor</p>
+                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                      <div class="text-center">
+                          <a href="#" class="btn btn-success">
+                            Gabung Kelas
+                          </a>
+                        </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="text-center">
+                  <img src="img/mentor3.png" alt="mentor" class="img-mentor">
+                  <p class="nama-mentor">Nama Mentor</p>
+                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                      <div class="text-center">
+                          <a href="#" class="btn btn-success">
+                            Gabung Kelas
+                          </a>
+                        </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="text-center">
+                  <img src="img/mentor1.png" alt="mentor" class="img-mentor">
+                  <p class="nama-mentor">Nama Mentor</p>
+                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                      <div class="text-center">
+                          <a href="#" class="btn btn-success">
+                            Gabung Kelas
+                          </a>
+                        </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="text-center">
+                  <img src="img/mentor2.png" alt="mentor" class="img-mentor">
+                  <p class="nama-mentor">Nama Mentor</p>
+                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                      <div class="text-center">
+                          <a href="#" class="btn btn-success">
+                            Gabung Kelas
+                          </a>
+                        </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="text-center">
+                  <img src="img/mentor3.png" alt="mentor" class="img-mentor">
+                  <p class="nama-mentor">Nama Mentor</p>
+                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                      <div class="text-center">
+                          <a href="#" class="btn btn-success">
+                            Gabung Kelas
+                          </a>
+                        </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="text-center">
+                  <img src="img/mentor1.png" alt="mentor" class="img-mentor">
+                  <p class="nama-mentor">Nama Mentor</p>
+                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                      <div class="text-center">
+                          <a href="#" class="btn btn-success">
+                            Gabung Kelas
+                          </a>
+                        </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="text-center">
+                  <img src="img/mentor2.png" alt="mentor" class="img-mentor">
+                  <p class="nama-mentor">Nama Mentor</p>
+                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                      <div class="text-center">
+                          <a href="#" class="btn btn-success">
+                            Gabung Kelas
+                          </a>
+                        </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="text-center">
+                  <img src="img/mentor3.png" alt="mentor" class="img-mentor">
+                  <p class="nama-mentor">Nama Mentor</p>
+                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
+                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
+                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
+                      <div class="text-center">
+                          <a href="#" class="btn btn-success">
+                            Gabung Kelas
+                          </a>
+                        </div>
+                </div>
+              </div>
+           
+            
+          </div>
+         
+          
+    </div>
+   </section>
+   <!-- end produk service -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</main>
+@endsection
