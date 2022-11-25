e2cc808243f5cb628a54c5595ce1b794a871c216
widyalestari9 <88524887+widyalestari9@users.noreply.github.com>
2022-11-25 12:02:35 +0700

    .

--- resources/views/pilih_mentor.blade.php
+++ resources/views/pilih_mentor.blade.php
@@ -20,166 +20,190 @@
 
         </div>
 
-
     </section>
-    <!-- End Hero Sction -->
-
-     <!-- produk service -->
-   <section class="service">
+    <section class="service">
     <div class="container"> 
-          <div class="row mt-5">
+          <div class="row">
 
             <div class="col-md-4">
-              <div class="text-center">
+              <div class="gambar">
                 <img src="img/mentor1.png" alt="mentor" class="img-mentor">
-                <p class="nama-mentor">Nama Mentor</p>
-                <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                    veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                    <div class="text-center">
-                        <a href="#" class="btn btn-success">
-                          Gabung Kelas
-                        </a>
-                      </div>
+              </div>
+            
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
               </div>
             </div>
 
             <div class="col-md-4">
-                <div class="text-center">
-                  <img src="img/mentor2.png" alt="mentor" class="img-mentor">
-                  <p class="nama-mentor">Nama Mentor</p>
-                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                      <div class="text-center">
-                          <a href="#" class="btn btn-success">
-                            Gabung Kelas
-                          </a>
-                        </div>
-                </div>
+              <div class="gambar">
+                <img src="img/mentor2.png" alt="mentor" class="img-mentor">
               </div>
-
-              <div class="col-md-4">
-                <div class="text-center">
-                  <img src="img/mentor3.png" alt="mentor" class="img-mentor">
-                  <p class="nama-mentor">Nama Mentor</p>
-                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                      <div class="text-center">
-                          <a href="#" class="btn btn-success">
-                            Gabung Kelas
-                          </a>
-                        </div>
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="text-center">
-                  <img src="img/mentor1.png" alt="mentor" class="img-mentor">
-                  <p class="nama-mentor">Nama Mentor</p>
-                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                      <div class="text-center">
-                          <a href="#" class="btn btn-success">
-                            Gabung Kelas
-                          </a>
-                        </div>
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="text-center">
-                  <img src="img/mentor2.png" alt="mentor" class="img-mentor">
-                  <p class="nama-mentor">Nama Mentor</p>
-                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                      <div class="text-center">
-                          <a href="#" class="btn btn-success">
-                            Gabung Kelas
-                          </a>
-                        </div>
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="text-center">
-                  <img src="img/mentor3.png" alt="mentor" class="img-mentor">
-                  <p class="nama-mentor">Nama Mentor</p>
-                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                      <div class="text-center">
-                          <a href="#" class="btn btn-success">
-                            Gabung Kelas
-                          </a>
-                        </div>
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="text-center">
-                  <img src="img/mentor1.png" alt="mentor" class="img-mentor">
-                  <p class="nama-mentor">Nama Mentor</p>
-                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                      <div class="text-center">
-                          <a href="#" class="btn btn-success">
-                            Gabung Kelas
-                          </a>
-                        </div>
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="text-center">
-                  <img src="img/mentor2.png" alt="mentor" class="img-mentor">
-                  <p class="nama-mentor">Nama Mentor</p>
-                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                      <div class="text-center">
-                          <a href="#" class="btn btn-success">
-                            Gabung Kelas
-                          </a>
-                        </div>
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="text-center">
-                  <img src="img/mentor3.png" alt="mentor" class="img-mentor">
-                  <p class="nama-mentor">Nama Mentor</p>
-                  <p class="bidang-pekerjaan">Bidang Pekerjaan</p>
-                  <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                      Possimus quod dolores, deserunt unde voluptas ratione omnis cupiditate 
-                      veritatis excepturi minus fugit nemo quo veniam facilis perferendis earum ipsum natus incidunt.</p>
-                      <div class="text-center">
-                          <a href="#" class="btn btn-success">
-                            Gabung Kelas
-                          </a>
-                        </div>
-                </div>
-              </div>
-           
             
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="gambar">
+                <img src="img/mentor3.png" alt="mentor" class="img-mentor">
+              </div>
+            
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="gambar">
+                <img src="img/mentor4.png" alt="mentor" class="img-mentor">
+              </div>
+            
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="gambar">
+                <img src="img/mentor5.png" alt="mentor" class="img-mentor">
+              </div>
+            
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="gambar">
+                <img src="img/mentor6.png" alt="mentor" class="img-mentor">
+              </div>
+            
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="gambar">
+                <img src="img/mentor1.png" alt="mentor" class="img-mentor">
+              </div>
+            
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="gambar">
+                <img src="img/mentor2.png" alt="mentor" class="img-mentor">
+              </div>
+            
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="gambar">
+                <img src="img/mentor3.png" alt="mentor" class="img-mentor">
+              </div>
+            
+              <div class="keteragan">
+                <p class="nama">Muhammad Nur Azizi</p>
+                <p class="bidang-pekerjaan">Frontend Web di Tokopedia</p>
+                <p class="desc">Lorem Ipsum is simply dummy text of the printing and tipesetting Industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley Load More.</p>                            
+              </div>
+              <div class="tombol">
+                <a href="#" class="btn btn-success">
+                  Gabung Kelas
+                </a>
+              </div>
+            </div> 
+           
+           
+         
           </div>
          
           
     </div>
-   </section>
-   <!-- end produk service -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+   </section> 
+
+   
 </main>
 @endsection
