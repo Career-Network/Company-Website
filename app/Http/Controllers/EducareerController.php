b44604a1060fbdc1cf06487570faa07a179572a4
irvanhanif <irhan8803@gmail.com>
2023-01-06 18:41:55 +0700

    add educareer controller

---
+++ app/Http/Controllers/EducareerController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EducareerController extends Controller
+{
+    public function index()
+    {
+        return view('list_class');
+    }
+    public function mentor()
+    {
+        return view('pilih_mentor');
+    }
+    public function kelas()
+    {
+        return view('detail-class');
+    }
+}
