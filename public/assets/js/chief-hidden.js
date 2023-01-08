e673605b4d3c9b3a42fa6714eb9ee15e4d574ef0
irvanhanif <irhan8803@gmail.com>
2023-01-08 14:29:23 +0700

    update organization section at about page

---
+++ public/assets/js/chief-hidden.js
@@ -0,0 +1,9 @@
+function showChief() {
+    const org = document.getElementsByClassName("organisasi")[0];
+    const chief = org.getElementsByClassName("card");
+    for(let i = 4; i < chief.length; i++){
+        chief[i].classList.toggle("d-none")
+    }
+    org.getElementsByClassName("button-txt")[0].getElementsByTagName("h4")[0].classList.toggle("active");
+    org.getElementsByClassName("row-btn")[0].classList.toggle("active");
+} 
