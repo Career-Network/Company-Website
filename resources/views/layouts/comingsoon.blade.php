c4183773a6438bdb78991326b0e3b4621c793f80
edwardtua25 <edwardtua25@gmail.com>
2023-02-07 11:54:22 +0700

    coming soon see

--- resources/views/layouts/comingsoon.blade.php
+++ resources/views/layouts/comingsoon.blade.php
@@ -1,4 +1,4 @@
-<section class="container-cs">
+<section class="container-cs" style="padding-top: 100px">
     <div class="coming-soon-container" style="background: url({{  asset('assets/img/coming-soon/bg-coming-soon.png') }})">
         <img class="cs-frame" src='{{ asset('assets/img/coming-soon/frame-coming-soon.png') }}' alt="Coming soon frame" />
         <img class="cs-illustration" src='{{ asset('assets/img/coming-soon/illustration-coming-soon.png') }}' alt="Coming soon illustration" />
