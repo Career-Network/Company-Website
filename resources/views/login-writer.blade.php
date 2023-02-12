e0ad9ef7bdea1ef8107292764bb00ed028da146f
gustonecrush <codeboxezz@gmail.com>
2023-02-13 00:21:09 +0700

    added tab icon

--- resources/views/login-writer.blade.php
+++ resources/views/login-writer.blade.php
@@ -6,6 +6,10 @@
     <link href="{{ asset('assets/img/logo.png') }}" rel="apple-touch-icon" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="{{ asset('assets/img/favico.png') }}" rel="icon" />
+    <link href="{{ asset('assets/img/logo.png') }}" rel="apple-touch-icon" />
+
     <link rel="stylesheet" href="{{ asset('assets/css/login_writer.css') }}">
     <title>
         Login Writer | Blog
