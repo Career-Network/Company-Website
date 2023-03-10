1c8622682ad65ffcd84a0384802ccdfe1d74117b
seccret404 <edwardtua25@gmail.com>
2023-03-10 23:09:40 +0700

    Push

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -134,6 +134,54 @@
             </div>
         </div>
     </section>
+    <section style="background-color: #ffff">
+        <h2 id="question">Apa kata mereka?</h2>
+        <p id="kren">Sangat senang belajar di career network!</p>
+        <div class="row container">
+            <div class="col-md-2 bg-secondary">p</div>
+            <div class="col-md-8 "><div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="tcb-quote-carousel">
+                          <div class="carousel slide carousel-fade" id="fade-quote-carousel" data-ride="carousel" data-interval="3000">
+                                <!-- Carousel indicators -->
+                                <ol class="carousel-indicators">
+                                    <li data-target="#fade-quote-carousel" data-slide-to="0" class="active"></li>
+                                    <li data-target="#fade-quote-carousel" data-slide-to="1"></li>
+                                  <li data-target="#fade-quote-carousel" data-slide-to="2"></li>
+                                </ol>
+                                <!-- Carousel items -->
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <div class="profile-circle"><img class="img-responsive " src="https://i.imgur.com/ctvpjSP.png" alt=""></div>
+                                        <blockquote>
+                                            <p>Welcome to Apex! <br> Now the site is running and open, we expect profiles to be done. There is a template you can fill in, instead of creating your own. Feel free to RP whenever, and start meeting people! </p>
+                                        </blockquote>
+                                    </div>
+                                    <div class="item">
+                                        <div class="profile-circle"><img class="img-responsive " src="https://i.imgur.com/hZFrbJJ.jpg" alt=""></div>
+                                        <blockquote>
+                                          <p> <I> Current Plot: </I>
+                                            <br>
+                                            Humans have been surviving against the animals ever since the first outbreak. Yet a cure has been found, and worked upon domestic dogs. This is the only species that is cured as for now. The cure is developing to spread across the whole animal kingdom and world. This is a slow process.</p>
+                                        </blockquote>
+                                    </div>
+                                    <div class="item">
+                                        <div class="profile-circle"><img class="img-responsive " src="https://i.imgur.com/e5TBLka.jpg" alt=""></div>
+                                        <blockquote>
+                                          <p><I> Bi-Weekly Challenge:</I>
+                                              <br> Moles have been getting into the underground system. Some areas are closed off due to weak or broken frame work. The community must find a way to remove of the moles and construct the buildings again. Points will be deducted, for any animals damaged, total points can be seen on the point page.</p>
+                                        </blockquote>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br />
+           </div></div>
+            <div class="col-md-2 bg-secondary">p</div>
+        </div>
+    </section>
 
     <section id="berita" >
         <div class="row">
@@ -142,7 +190,7 @@
                 <p class="bt_3">Nikmati Cerita, Hiburan, Informasi serta Promo menarik dari  blog Career Network. </p>
                 <button><a href="{{ route('blog') }}" class="link">Lihat Lebih Banyak</a></button>
             </div>
-            <div class="col-sm-7">  
+            <div class="col-sm-7">
                 <div class="slider owl-carousel">
                     <div class="carddy-1">
                         <div class="img">
